76649e22a7a19d3765c76cb58e6caeed97f7eade
黑牛12138 <2469000189@qq.com>
2020-09-03 21:52:07 +0800

    添加退出登录功能

--- src/views/index/components/header.js
+++ src/views/index/components/header.js
@@ -1,14 +1,20 @@
 import React, { Component, Fragment} from 'react';
 
 import {  MenuFoldOutlined  } from '@ant-design/icons';
+import { withRouter } from 'react-router-dom'
 
 import './aside.scss'
+
+import { Menu, Dropdown } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
+import { Modal, Button } from 'antd';
 
 class Header extends Component {
     constructor(props){
         super(props);
         this.state = {
-            collapsed : props.collapsed
+            collapsed : props.collapsed,
+            visible: false
         }
     }
 
@@ -17,20 +23,60 @@ class Header extends Component {
         this.props.toggle()
     }
 
+    menu = () => {
+        return (
+            <Menu>
+              <Menu.Item danger onClick={this.onhandelShow}>退出</Menu.Item>
+            </Menu>
+          );
+    } 
+
+    onhandelShow = () => {
+        
+        this.setState({
+            visible: true,
+        })
+    }
+
+    handleOk = e => {
+        console.log(e);
+        this.props.history.push({ pathname: '/'})
+        this.setState({
+          visible: false,
+        });
+      };
+    
+      handleCancel = e => {
+        console.log(e);
+        this.setState({
+          visible: false,
+        });
+      };
+      
+
     render (){
         return (
             <Fragment>
                 <span onClick={this.toggleMenu} className="collapsed-icon">
                     <MenuFoldOutlined />
                 </span>
+                <Dropdown overlay={this.menu} className="setting">
+                    <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+                    Hover me <DownOutlined />
+                    </a>
+                </Dropdown>
+                <Modal
+                    title="系统提示"
+                    visible={this.state.visible}
+                    onOk={this.handleOk}
+                    onCancel={this.handleCancel}
+                    >
+                    <p>您确定要退出登录吗？</p>
+                    </Modal>
             </Fragment>
         )
     }
 }
 
-export default Header
-
-
-
-
+export default withRouter(Header);
 
